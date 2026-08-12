43ee7f623c36c2084c6026ee3d5d29ecd8404ed9
Karina Vascarin <srtkmenezes@gmail.com>
2026-08-12 13:33:47 -0300

    Adiciona rodapé e políticas de troca na página principal

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,27 @@ import Catalog from '@/components/Catalog';
 import FloatingActions from '@/components/FloatingActions';
 import FloatingCart from '@/components/FloatingCart';
 import FloatingFavorites from '@/components/FloatingFavorites';
+import Footer from '@/components/Footer';
 import { getProducts } from '@/lib/products';
 
 export default async function Home() {
   const products = await getProducts();
 
   return (
-    <main className="min-h-screen bg-white dark:bg-zinc-950 relative">
-      <Catalog initialProducts={products} />
-      
-      {/* Botões flutuantes no canto da tela */}
-      <FloatingActions />
-      
-      {/* Painéis laterais da sacola e favoritos (controlados pelo Zustand) */}
-      <FloatingCart />
-      <FloatingFavorites />
+    <main className="min-h-screen bg-white dark:bg-zinc-950 relative flex flex-col justify-between">
+      <div>
+        <Catalog initialProducts={products} />
+        
+        {/* Botões flutuantes no canto da tela */}
+        <FloatingActions />
+        
+        {/* Painéis laterais da sacola e favoritos (controlados pelo Zustand) */}
+        <FloatingCart />
+        <FloatingFavorites />
+      </div>
+
+      {/* Rodapé fixo com a política de trocas, devoluções e pop-up */}
+      <Footer />
     </main>
   );
 }
